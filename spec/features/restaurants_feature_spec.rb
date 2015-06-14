9b60412496877e1e15f8e0f826469ef21aeefc56
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-14 23:11:24 +0100

    four tests passed

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -6,6 +6,20 @@ feature 'restaurants' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
+      expect(page).to have_content 'Yelp Clone'
     end
   end
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+  end
+
 end
